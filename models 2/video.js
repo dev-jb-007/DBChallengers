5577e54c60b655e95c0ccb1193269c9e763e0834
Dev Patel <devpatel8907@gmail.com>
2022-01-09 05:16:34 +0530

    9 jan 5:16 AM

--- models 2/video.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-const Schema = mongoose.Schema;
-
-const videoSchema = new Schema({
-    file:{
-        type:Buffer
-    }
-})
-module.exports = mongoose.model('Video', videoSchema);
